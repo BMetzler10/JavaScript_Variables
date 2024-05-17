839db2d5256c9892cc79288536e01664f901bfa1
brianna metzler <brianna.metzler10@gmail.com>
2024-05-17 11:27:37 -0400

    Updated JS declaring variables

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Mickey";
+let lastName = "Mouse";
+var age = 1923;
 
-// Exercise 2
+lastName = "Minnie";
+age = 100;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1955;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+let product = "T-shirty";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} in size ${selectedSize} is $${price} with ${stock} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722; 
+let bookmark = 456;
+let hasread = true; 
+
+console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`)
